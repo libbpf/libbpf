103fc6ec4171a3daa1fa8f871fe969b5f665e96a
Andrii Nakryiko <andriin@fb.com>
2019-10-01 09:48:58 -0700

    ci: execute install step in CI
    Add simple execution of `make install` in Debian and Xenial build to
    catch most obvious breakages.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- travis-ci/managers/xenial.sh
+++ travis-ci/managers/xenial.sh
@@ -11,7 +11,7 @@ source "$(dirname $0)/travis_wait.bash"
 cd $REPO_ROOT
 
 CFLAGS="-g -O2 -Werror -Wall -fsanitize=address,undefined"
-mkdir build
+mkdir build install
 cc --version
 make CFLAGS="${CFLAGS}" -C ./src -B OBJDIR=../build
 ldd build/libbpf.so
@@ -19,4 +19,5 @@ if ! ldd build/libbpf.so | grep -q libelf; then
     echo "FAIL: No reference to libelf.so in libbpf.so!"
     exit 1
 fi
-rm -rf build
+make -C src OBJDIR=../build DESTDIR=../install install
+rm -rf build install
