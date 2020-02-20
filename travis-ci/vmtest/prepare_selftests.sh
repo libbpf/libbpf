1feb21b0816e18be790aa4bbc6f8159f1feefbe7
Andrii Nakryiko <andriin@fb.com>
2020-02-20 11:48:22 -0800

    vmtest: remove temporary runqslower fix
    It's now in bpf-next and this work around is not needed anymore.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- travis-ci/vmtest/prepare_selftests.sh
+++ travis-ci/vmtest/prepare_selftests.sh
@@ -12,8 +12,3 @@ if [[ "${KERNEL}" = 'LATEST' ]]; then
 	make -j $((4*$(nproc))) olddefconfig all
 fi
 
-# Fix runqslower build
-# TODO(hex@): remove after the patch is merged from bpf to bpf-next tree
-wget https://lore.kernel.org/bpf/908498f794661c44dca54da9e09dc0c382df6fcb.1580425879.git.hex@fb.com/t.mbox.gz
-gunzip t.mbox.gz
-git apply t.mbox
