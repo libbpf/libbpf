22d5d404935e019d02d62ff999a165c5e2650799
Andrii Nakryiko <andriin@fb.com>
2020-02-21 15:11:54 -0800

    ci: fetch and build latest pahole
    Build latest pahole from sources and not rely on hacky Ubuntu repository
    approach.
    Also enable tests for latest kernel that rely on pahole 1.16.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

---
+++ travis-ci/vmtest/build_pahole.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eux
+
+CWD=$(pwd)
+REPO_PATH=$1
+PAHOLE_ORIGIN=https://git.kernel.org/pub/scm/devel/pahole/pahole.git
+
+mkdir -p ${REPO_PATH}
+cd ${REPO_PATH}
+git init
+git remote add origin ${PAHOLE_ORIGIN}
+git fetch origin
+git checkout master
+
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -D__LIB=lib ..
+make -j$((4*$(nproc))) all
+sudo make install
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/usr/local/lib
+ldd $(which pahole)
+pahole --version
+
