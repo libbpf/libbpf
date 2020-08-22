eb90c57de4d3b8aa67432ba4a730843d1bb2796e
Andrii Nakryiko <andriin@fb.com>
2020-08-21 19:06:43 -0700

    vmtests: add log folding
    Sprinkle log folds around, including timing.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- travis-ci/vmtest/run_selftests.sh
+++ travis-ci/vmtest/run_selftests.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
+
+source $(cd $(dirname $0) && pwd)/helpers.sh
 
 test_progs() {
 	if [[ "${KERNEL}" != '4.9.0' ]]; then
-		echo TEST_PROGS
+		travis_fold start test_progs "Testing test_progs"
 		./test_progs ${BLACKLIST:+-b$BLACKLIST} ${WHITELIST:+-t$WHITELIST}
+		travis_fold end test_progs
 	fi
 
-	echo TEST_PROGS-NO_ALU32
+	travis_fold start test_progs-no_alu32 "Testing test_progs-no_alu32"
 	./test_progs-no_alu32 ${BLACKLIST:+-b$BLACKLIST} ${WHITELIST:+-t$WHITELIST}
+	travis_fold end test_progs-no_alu32
 }
 
 test_maps() {
-	echo TEST_MAPS
+	travis_fold start test_maps "Testing test_maps"
 	./test_maps
+	travis_fold end test_maps
 }
 
 test_verifier() {
-	echo TEST_VERIFIER
+	travis_fold start test_verifier "Testing test_verifier"
 	./test_verifier
+	travis_fold end test_verifier
 }
+
+travis_fold end vm_init
 
 configs_path='libbpf/travis-ci/vmtest/configs'
 blacklist_path="$configs_path/blacklist/BLACKLIST-${KERNEL}"
