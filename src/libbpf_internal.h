d8d138c5dc718d74a96ee08638dae7caecc840cd
Patrick McCarty <patrick.mccarty@intel.com>
2021-01-03 06:35:18 +0000

    Fix build with latest gcc/binutils with LTO
    After updating to binutils 2.35, the build began to fail with an
    assembler error. A bug was opened on the Red Hat Bugzilla a few days
    later for the same issue.
    
    Work around the problem by using the new `symver` attribute (introduced
    in GCC 10) as needed, instead of the `COMPAT_VERSION` and
    `DEFAULT_VERSION` macros, which expand to assembler directives.
    
    Fixes: https://github.com/libbpf/libbpf/issues/338
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1863059
    Fixes: https://bugzilla.opensuse.org/show_bug.cgi?id=1188749
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>
    Make the change conditional on GCC version
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>

--- src/libbpf_internal.h
+++ src/libbpf_internal.h
@@ -87,20 +87,31 @@
 	(offsetof(TYPE, FIELD) + sizeof(((TYPE *)0)->FIELD))
 #endif
 
+#ifdef __GNUC__
+# if __GNUC__ >= 10
+#  define DEFAULT_VERSION(internal_name, api_name, version) \
+__attribute__((__symver__(#api_name "@@" #version)))
+#  define COMPAT_VERSION(internal_name, api_name, version) \
+__attribute__((__symver__(#api_name "@" #version)))
+# endif
+#endif
+
+#if ! defined(COMPAT_VERSION) || ! defined(DEFAULT_VERSION)
 /* Symbol versioning is different between static and shared library.
  * Properly versioned symbols are needed for shared library, but
  * only the symbol of the new version is needed for static library.
  */
-#ifdef SHARED
-# define COMPAT_VERSION(internal_name, api_name, version) \
+# ifdef SHARED
+#  define COMPAT_VERSION(internal_name, api_name, version) \
 	asm(".symver " #internal_name "," #api_name "@" #version);
-# define DEFAULT_VERSION(internal_name, api_name, version) \
+#  define DEFAULT_VERSION(internal_name, api_name, version) \
 	asm(".symver " #internal_name "," #api_name "@@" #version);
-#else
-# define COMPAT_VERSION(internal_name, api_name, version)
-# define DEFAULT_VERSION(internal_name, api_name, version) \
+# else
+#  define COMPAT_VERSION(internal_name, api_name, version)
+#  define DEFAULT_VERSION(internal_name, api_name, version) \
 	extern typeof(internal_name) api_name \
 	__attribute__((alias(#internal_name)));
+# endif
 #endif
 
 extern void libbpf_print(enum libbpf_print_level level,
