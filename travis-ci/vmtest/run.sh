5d5af3f07e7599fe278e1c74a639e8b762c8bf2b
Sergei Iudin <tsipa740@gmail.com>
2021-06-14 11:16:46 -0700

    Migrate libbpf ci to GH actions
    changes to docker command require to run it in non-interactive mode

--- travis-ci/vmtest/run.sh
+++ travis-ci/vmtest/run.sh
@@ -436,11 +436,15 @@ sudo umount "$mnt"
 
 echo "Starting VM with $(nproc) CPUs..."
 
+if kvm-ok ; then
+  accel="-cpu kvm64 -enable-kvm"
+else
+  accel="-cpu qemu64 -machine accel=tcg"
+fi
 qemu-system-x86_64 -nodefaults -display none -serial mon:stdio \
-	-cpu kvm64 -enable-kvm -smp "$(nproc)" -m 4G \
-	-drive file="$IMG",format=raw,index=1,media=disk,if=virtio,cache=none \
-	-kernel "$vmlinuz" -append "root=/dev/vda rw console=ttyS0,115200$APPEND"
-
+  ${accel} -smp "$(nproc)" -m 4G \
+  -drive file="$IMG",format=raw,index=1,media=disk,if=virtio,cache=none \
+  -kernel "$vmlinuz" -append "root=/dev/vda rw console=ttyS0,115200$APPEND"
 sudo mount -o loop "$IMG" "$mnt"
 if exitstatus="$(cat "$mnt/exitstatus" 2>/dev/null)"; then
 	printf '\nTests exit status: %s\n' "$exitstatus" >&2
