cdef8257a897b3bb8ba5c10603af4a61a324ec69
Andrii Nakryiko <andrii@kernel.org>
2022-02-04 15:28:06 -0800

    ci: bump LLVM_VER to 15
    Another bump of latest clang version to be used during BPF selftests build.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- .github/actions/build-selftests/build_selftests.sh
+++ .github/actions/build-selftests/build_selftests.sh
@@ -8,7 +8,7 @@ source ${THISDIR}/helpers.sh
 
 travis_fold start prepare_selftests "Building selftests"
 
-LLVM_VER=14
+LLVM_VER=15
 LIBBPF_PATH="${REPO_ROOT}"
 
 PREPARE_SELFTESTS_SCRIPT=${THISDIR}/prepare_selftests-${KERNEL}.sh
