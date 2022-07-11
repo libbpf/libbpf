f11d42d2ff299c19d5a5d02ce6f69345aaa1b8cc
Daniel Müller <deso@posteo.net>
2022-07-11 14:43:25 -0700

    Rename travis_fold function to foldable
    We are no longer using Travis. As such, it is confusing to anyone
    reading the code to see a function prefixed 'travis_' in GitHub actions
    code.
    This change renames the travis_fold function to 'foldable', as a first
    step towards eliminating such confusing constructs from the repository
    where possible.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- travis-ci/vmtest/helpers.sh
+++ travis-ci/vmtest/helpers.sh
@@ -1,7 +1,7 @@
 # $1 - start or end
 # $2 - fold identifier, no spaces
 # $3 - fold section description
-travis_fold() {
+foldable() {
   local YELLOW='\033[1;33m'
   local NOCOLOR='\033[0m'
   if [ -z ${GITHUB_WORKFLOW+x} ]; then
--- travis-ci/vmtest/run_selftests.sh
+++ travis-ci/vmtest/run_selftests.sh
@@ -18,36 +18,36 @@ read_lists() {
 
 test_progs() {
 	if [[ "${KERNEL}" != '4.9.0' ]]; then
-		travis_fold start test_progs "Testing test_progs"
+		foldable start test_progs "Testing test_progs"
 		# "&& true" does not change the return code (it is not executed
 		# if the Python script fails), but it prevents exiting on a
 		# failure due to the "set -e".
 		./test_progs ${BLACKLIST:+-d$BLACKLIST} ${WHITELIST:+-a$WHITELIST} && true
 		echo "test_progs:$?" >> "${STATUS_FILE}"
-		travis_fold end test_progs
+		foldable end test_progs
 	fi
 
-	travis_fold start test_progs-no_alu32 "Testing test_progs-no_alu32"
+	foldable start test_progs-no_alu32 "Testing test_progs-no_alu32"
 	./test_progs-no_alu32 ${BLACKLIST:+-d$BLACKLIST} ${WHITELIST:+-a$WHITELIST} && true
 	echo "test_progs-no_alu32:$?" >> "${STATUS_FILE}"
-	travis_fold end test_progs-no_alu32
+	foldable end test_progs-no_alu32
 }
 
 test_maps() {
-	travis_fold start test_maps "Testing test_maps"
+	foldable start test_maps "Testing test_maps"
 	./test_maps && true
 	echo "test_maps:$?" >> "${STATUS_FILE}"
-	travis_fold end test_maps
+	foldable end test_maps
 }
 
 test_verifier() {
-	travis_fold start test_verifier "Testing test_verifier"
+	foldable start test_verifier "Testing test_verifier"
 	./test_verifier && true
 	echo "test_verifier:$?" >> "${STATUS_FILE}"
-	travis_fold end test_verifier
+	foldable end test_verifier
 }
 
-travis_fold end vm_init
+foldable end vm_init
 
 configs_path=${PROJECT_NAME}/vmtest/configs
 BLACKLIST=$(read_lists "$configs_path/blacklist/BLACKLIST-${KERNEL}" "$configs_path/blacklist/BLACKLIST-${KERNEL}.${ARCH}")
