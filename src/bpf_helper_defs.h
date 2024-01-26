0e57fade4eb9ced399a43baf945663364bb02c2a
Andrii Nakryiko <andrii@kernel.org>
2024-01-25 18:33:27 -0800

    sync: auto-generate latest BPF helpers
    Latest changes to BPF helper definitions.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- src/bpf_helper_defs.h
+++ src/bpf_helper_defs.h
@@ -3695,9 +3695,9 @@ static void *(*bpf_this_cpu_ptr)(const void *percpu_ptr) = (void *) 154;
  * 	going through the CPU's backlog queue.
  *
  * 	The *flags* argument is reserved and must be 0. The helper is
- * 	currently only supported for tc BPF program types at the ingress
- * 	hook and for veth device types. The peer device must reside in a
- * 	different network namespace.
+ * 	currently only supported for tc BPF program types at the
+ * 	ingress hook and for veth and netkit target device types. The
+ * 	peer device must reside in a different network namespace.
  *
  * Returns
  * 	The helper returns **TC_ACT_REDIRECT** on success or
