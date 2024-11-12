a5977b4fd5b9ba4e0791485084e3bcb13549cd12
Ihor Solodrai <ihor.solodrai@pm.me>
2024-11-12 13:43:31 -0800

    ci: switch to libbpf/ci/build-selftests action
    Signed-off-by: Ihor Solodrai <ihor.solodrai@pm.me>

---
+++ ci/vmtest/prepare-selftests-build-5.5.0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "${SELFTESTS_BPF}"
+printf "all:\n\ttouch bpf_testmod.ko\n\nclean:\n" > bpf_testmod/Makefile
+printf "all:\n\ttouch bpf_test_no_cfi.ko\n\nclean:\n" > bpf_test_no_cfi/Makefile
+
